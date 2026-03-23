7af6506cd0a5835ad2233219f0b33c2c0a89c210
fzozo <fulopzoli95@gmail.com>
2026-03-23 20:04:13 +0100

    Added variables for incoming values

--- ctrl_long_emergency/include/ctrl_long_emergency/ctrlLongEmergency.hpp
+++ ctrl_long_emergency/include/ctrl_long_emergency/ctrlLongEmergency.hpp
@@ -1,3 +1,6 @@
+#ifndef CTRL_LONG_EMERGENCY_HPP
+#define CTRL_LONG_EMERGENCY_HPP
+
 #include "rclcpp/rclcpp.hpp"
 #include "autoware_planning_msgs/msg/trajectory.hpp"
 #include "crp_msgs/msg/ego.hpp"
@@ -22,6 +25,14 @@ private:
     rclcpp::Publisher<autoware_control_msgs::msg::Control>::SharedPtr m_pubControl_;
 
     rclcpp::TimerBase::SharedPtr m_timer_;
+
+    // values coming from trajectory msg
+    double m_trajectoryVelocity;
+    long m_trajectoryTime;
+
+    // values coming from ego msg
+    double m_egoVelocity;
 };
 
 } // namespace cle
+#endif // CTRL_LONG_EMERGENCY_HPP
--- ctrl_long_emergency/src/ctrlLongEmergency.cpp
+++ ctrl_long_emergency/src/ctrlLongEmergency.cpp
@@ -28,23 +28,28 @@ cle::CtrlLongEmergency::CtrlLongEmergency() : Node("control_publisher")
 
 void cle::CtrlLongEmergency::trajectoryCallback(const autoware_planning_msgs::msg::Trajectory::SharedPtr msg)
 {
+    m_trajectoryVelocity = msg->points[-1].longitudinal_velocity_mps;
+    m_trajectoryTime = msg->points[-1].time_from_start.sec;
+
     // read debug param
     bool isDebugEnabled;
     this->get_parameter<bool>("debug_enabled", isDebugEnabled);
 
     if (isDebugEnabled)
-        RCLCPP_INFO(this->get_logger(), "Received trajectory message! Time: %d", msg->header.stamp.sec);
+        RCLCPP_INFO(this->get_logger(), "Received trajectory message! Time: %d, Velocity: %f, Time from start: %d", 
+            msg->header.stamp.sec, m_trajectoryVelocity, m_trajectoryTime);
 }
 
 void cle::CtrlLongEmergency::egoCallback(const crp_msgs::msg::Ego::SharedPtr msg)
 {
+    m_egoVelocity = msg->twist.twist.linear.x;
+
     // read debug param
-    double egoVelocity = msg->twist.twist.linear.x;
     bool isDebugEnabled;
     this->get_parameter<bool>("debug_enabled", isDebugEnabled);
 
     if (isDebugEnabled)
-        RCLCPP_INFO(this->get_logger(), "Received ego message! Time: %d, Velocity: %f", msg->header.stamp.sec, egoVelocity);
+        RCLCPP_INFO(this->get_logger(), "Received ego message! Time: %d, Velocity: %f", msg->header.stamp.sec, m_egoVelocity);
 }
 
 void cle::CtrlLongEmergency::timerCallback()
@@ -52,7 +57,11 @@ void cle::CtrlLongEmergency::timerCallback()
     // create message and init with current time
     autoware_control_msgs::msg::Control controlMsg;
     controlMsg.stamp = this->get_clock()->now();
-    controlMsg.longitudinal.velocity = 0.0f; // set velocity to 0 for emergency braking
+
+    // TODO: target speed calculation for the next 20ms
+    
+
+    controlMsg.longitudinal.velocity = m_trajectoryVelocity; // target speed
 
     // publish the message
     m_pubControl_->publish(controlMsg);
