9ff9529835d7c37d5502a1d28f190a9b4232c711
Timot <hgtimot@gmail.com>
2026-04-25 17:59:24 +0200

    Launch fájl helyreállítva

--- system_launch.py
+++ system_launch.py
@@ -3,24 +3,24 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # 1. Döntéshozó Node
+        # 1. A Behavior Planner node indítása
         Node(
             package='behavior_planner',
             executable='behavior_planner_node',
             name='behavior_planner',
             output='screen' # Így a logok egy terminálba ömlenek
         ),
-        # 2. Tervező Node (A te kódod)
+        # 2. A Plan Long Emergency node indítása
         Node(
             package='plan_long_emergency',
             executable='plan_long_emergency_node',
             name='plan_long_emergency',
             output='screen'
         ),
-        # 3. Végrehajtó (Control) Node
+        # 3. A Control Long Emergency node indítása
         Node(
             package='ctrl_long_emergency',
-            executable='ctrl_long_emergency',
+            executable='ctrl_long_emergency_node',
             name='ctrl_long_emergency',
             output='screen'
         )
